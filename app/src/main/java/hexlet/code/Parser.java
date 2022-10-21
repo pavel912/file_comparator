63ddde61a60a35e368a470cd49f57d2cf914781d
pavel912 <gta4thebest@mail.ru>
2022-10-21 23:56:40 +0300

    add separate class for reading data and refactor related classes

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,26 +1,22 @@
 package hexlet.code;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 public class Parser {
-    public static Map<String, Object> getData(String filepath) throws Exception {
-        Path absFilepath = Paths.get(filepath).toAbsolutePath();
-        String fileContent = Files.readString(absFilepath);
+    public static Map<String, Object> parseData(String fileContent, String fileFormat) throws Exception {
 
-        if (absFilepath.toString().endsWith(".json")) {
-            ObjectMapper jsonMapper = new ObjectMapper();
-            return jsonMapper.readValue(fileContent, Map.class);
-        } else if (absFilepath.toString().endsWith(".yaml") || absFilepath.toString().endsWith(".yml")) {
-            ObjectMapper yamlMapper = new YAMLMapper();
-            return yamlMapper.readValue(fileContent, Map.class);
+        switch (fileFormat) {
+            case "json" -> {
+                ObjectMapper jsonMapper = new ObjectMapper();
+                return jsonMapper.readValue(fileContent, Map.class);
+            }
+            case "yaml" -> {
+                ObjectMapper yamlMapper = new YAMLMapper();
+                return yamlMapper.readValue(fileContent, Map.class);
+            }
+            default -> throw new Exception("Wrong file format. Please use only json and yaml files");
         }
-
-        throw new Exception("Wrong file format. Please use only json and yaml files");
     }
 }
